e2358dea18a438f72a17f42d767774f03efe3543
Alwin Dávila <edudavilaraffo@gmail.com>
2023-11-21 03:34:38 -0500

    Fixing another problemita

--- src/js/profile/main.js
+++ src/js/profile/main.js
@@ -1,12 +1,12 @@
 import '/src/css/globals.css'
 import '/src/css/profile.css'
 
-const toggleButton = document.getElementsByClassName('nav-toggle-button')[0]
-const navBarLinks = document.getElementsByClassName('nav-bar-links')[0]
+// const toggleButton = document.getElementsByClassName('nav-toggle-button')[0]
+// const navBarLinks = document.getElementsByClassName('nav-bar-links')[0]
 
-toggleButton.addEventListener('click', () => {
-    navBarLinks.classList.toggle('active')
-}) 
+// toggleButton.addEventListener('click', () => {
+//     navBarLinks.classList.toggle('active')
+// }) 
 
 document.getElementById('ft1').addEventListener('click', function() {
     console.log('Clic en ft1');
