c208143e0757aafe3c9fdb8a655594e106312f50
Paolo Flores <pflores.fisi22@gmail.com>
2023-11-21 13:13:03 -0500

    fix paths to images

--- src/js/profile/main.js
+++ src/js/profile/main.js
@@ -6,33 +6,33 @@ import '/src/css/profile.css'
 
 // toggleButton.addEventListener('click', () => {
 //     navBarLinks.classList.toggle('active')
-// }) 
+// })
 
 document.getElementById('ft1').addEventListener('click', function() {
     console.log('Clic en ft1');
-    openModal('../../../public/misias1.jpg');
+    openModal('/misias1.jpg');
   });
 
   document.getElementById('ft2').addEventListener('click', function() {
     console.log('Clic en ft2');
-    openModal('../../../public/misias2.avif');
+    openModal('/misias2.avif');
   });
 
   document.getElementById('ft3').addEventListener('click', function() {
     console.log('Clic en ft3');
-    openModal('../../../public/misias3.avif');
+    openModal('/misias3.avif');
   });
 
   document.getElementById('ft4').addEventListener('click', function() {
     console.log('Clic en ft4');
-    openModal('../../../public/misias4.avif');
+    openModal('/misias4.avif');
   });
 
 function openModal(imageSrc) {
     const modal = document.getElementById('myModal');
     const modalImage = document.getElementById('modalImage');
     currentImageIndex = getImageIndex(imageSrc);
-  
+
     modal.style.display = 'block';
     modalImage.src = imageSrc;
   }
