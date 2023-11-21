95fce2dc7cbe48159970397327308da38e6cd243
Alwin Dávila <edudavilaraffo@gmail.com>
2023-11-21 02:51:08 -0500

    Feat: profile page

--- src/js/profile/main.js
+++ src/js/profile/main.js
@@ -1,24 +1,31 @@
 import '/src/css/globals.css'
 import '/src/css/profile.css'
 
+const toggleButton = document.getElementsByClassName('nav-toggle-button')[0]
+const navBarLinks = document.getElementsByClassName('nav-bar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+    navBarLinks.classList.toggle('active')
+}) 
+
 document.getElementById('ft1').addEventListener('click', function() {
     console.log('Clic en ft1');
-    openModal('../../../public/imagenEjemplo.jpg');
+    openModal('../../../public/misias1.jpg');
   });
 
   document.getElementById('ft2').addEventListener('click', function() {
     console.log('Clic en ft2');
-    openModal('../../../public/holi.png');
+    openModal('../../../public/misias2.avif');
   });
 
   document.getElementById('ft3').addEventListener('click', function() {
     console.log('Clic en ft3');
-    openModal('../../../public/imagenEjemplo.jpg');
+    openModal('../../../public/misias3.avif');
   });
 
   document.getElementById('ft4').addEventListener('click', function() {
     console.log('Clic en ft4');
-    openModal('../../../public/imagenEjemplo.jpg');
+    openModal('../../../public/misias4.avif');
   });
 
 function openModal(imageSrc) {
