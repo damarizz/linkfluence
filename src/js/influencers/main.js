159d67327924ace73c049ea2ab763ede2a9317f3
damarizz <138645520+damarizz@users.noreply.github.com>
2023-11-21 01:06:06 -0500

    finishing influencers page

--- src/js/influencers/main.js
+++ src/js/influencers/main.js
@@ -1,1 +1,9 @@
 import '/src/css/globals.css'
+import '/src/css/influencers.css'
+
+const toggleButton = document.getElementsByClassName('nav-toggle-button')[0]
+const navBarLinks = document.getElementsByClassName('nav-bar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+    navBarLinks.classList.toggle('active')
+})
