1a38400b2735af4adeeadccce22c03070144ebc6
paoloose <pflores.fisi22@gmail.com>
2023-11-21 19:40:49 -0500

    add navbar functionality

--- src/js/main.js
+++ src/js/main.js
@@ -124,3 +124,10 @@ influencers.forEach((influencer) => {
   `
   feed.appendChild(element)
 })
+
+const toggleButton = document.getElementsByClassName('nav-toggle-button')[0]
+const navBarLinks = document.getElementsByClassName('nav-bar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+    navBarLinks.classList.toggle('active')
+})
